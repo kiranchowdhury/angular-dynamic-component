f7caaa99e164e3df3869782da89ea0483b8ae26b
kiranchowdhury <kiran.soft@gmail.com>
2019-06-13 00:44:09 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {ListBoxComponent} from './list-box/list-box.component';
+import {TextBoxComponent} from './text-box/text-box.component';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -17,8 +18,20 @@ export class AppComponent  {
       component: ListBoxComponent,
       inputs: payload
     }
+    setTimeout(() => {this.createTextComponent()}, 0)
   }
 
-  createTextComponent() {}
+  createTextComponent() {
+    const payload = {
+      searchString: '',
+      helpText: 'Multiple quotes search is allowd'
+    }
+    this.componentData = {
+      component: TextBoxComponent,
+      inputs: payload
+    }
+  }
+
+
 
 }
