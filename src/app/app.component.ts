2c75d36b56d5b1fb825bcbc958b438872b5ba7c9
kiranchowdhury <kiran.soft@gmail.com>
2019-06-13 00:35:26 -0400

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import {ListBoxComponent} from './list-box/list-box.component';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  componentData = null;
+
+  createListComponent() {
+    const payload = {
+      items: ['Quote Id(s)', 'Approval Code', 'Approval Date'],
+      defaultItem:  'Quote Id(s)'
+    }
+    this.componentData = {
+      component: ListBoxComponent,
+      inputs: payload
+    }
+  }
+
+  createTextComponent() {}
+
 }
