09c5a15b08a682470e3951cd94066f46232120ba
kiranchowdhury <kiran.soft@gmail.com>
2019-06-12 22:29:54 -0400

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CriteriaListComponent } from './criteria-list/criteria-list.component';
 import { ListBoxComponent } from './list-box/list-box.component';
 import { TextBoxComponent } from './text-box/text-box.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, CriteriaListComponent, ListBoxComponent, TextBoxComponent ],
-  entryComponents: [CriteriaListComponent, ListBoxComponent, TextBoxComponent],
+  declarations: [ AppComponent, ListBoxComponent, TextBoxComponent ],
+  entryComponents: [ListBoxComponent, TextBoxComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
